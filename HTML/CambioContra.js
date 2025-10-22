47f52f916f20aabf8fec4558b77749e0e8ca41c5
pelucacodiloco123 <lcalivari@gmail.com>
2025-10-22 17:25:57 -0300

    Add files via upload

--- HTML/CambioContra.js
+++ HTML/CambioContra.js
@@ -1,8 +1,9 @@
-const form = document.querySelector("#resetForm"); //El form del cambiocontra tiene este id
+const form = document.querySelector("#resetForm"); //El form del cambiocontra tiene este id, haciendo que lo agarre con el queryselector
 const mensaje = document.querySelector("#resultado1"); //Este es el mensaje de error
+//Document representa la pagina actual en la que nos encontramos. # es para el id, . para una clase
 
-form.addEventListener("submit", async (e) => { //Indica que va a haber una espera
-  e.preventDefault();
+form.addEventListener("submit", async (event) => { //Indica que va a haber una espera
+  event.preventDefault();
 
   const data = Object.fromEntries(new FormData(form));
 
@@ -12,32 +13,42 @@ form.addEventListener("submit", async (e) => { //Indica que va a haber una esper
     return;
   }
 
-  const body = {
+  const body = { //Se crea un objecto body que es lo que se va a enviar al servidor para actualizarlo
     contacto: data.contacto,
     password: data.password,
   };
+  const RESTAPI = {
+		resetCliente: 'http://localhost:8080/api/resetCliente',
+	};
 
-  try {
-    const res = await fetch("http://localhost:8080/api/resetCliente", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
+	// Configuración del fetch
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body), //transforma el body en json
+	};
 
-    const json = await res.json();
+fetch(RESTAPI.resetCliente, options)
+		.then((res) => res.json()) //toma la respuesta de json y lo transforma en un objecto
+		.then((response) => {
+			console.log('Respuesta del servidor:', response);
 
-    if (json.response === "OK") {
-      mensaje.className = "exito";
-      mensaje.style.color = "green";
-      mensaje.textContent = "Contraseña actualizada.";
-     (window.location.href = "loginClient.html");
-    } else {
-      mensaje.className = "mensaje";
-      mensaje.style.color = "red";
-      mensaje.textContent = json.message || "Error al actualizar";
-    }
-  } catch (err) {
-    mensaje.style.color = "red";
-    mensaje.textContent = "Error de red: " + err;
-  }
+			if (response.response === 'OK') {
+				//  Registro exitoso
+				window.location.href = 'loginClient.html';
+			} else {
+				//  Error
+				document.getElementById('resultado1').style.color = 'RED';
+				document.getElementById('resultado1').textContent =
+					response.message || 'Error al registrar cliente.';
+			}
+		})
+		.catch((err) => {
+			console.error('Error en el fetch:', err);
+			document.getElementById('resultado1').style.color = 'RED';
+			document.getElementById('resultado1').textContent =
+				'Error de conexión con el servidor.';
+		});
 });
